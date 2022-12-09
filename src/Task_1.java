70b1e419c88efd91a55726278fd004d044fd0e88
-MaxPank- <101281041+MaxPankratov@users.noreply.github.com>
2022-12-09 17:41:47 +0300

    Update Task_1.java

--- src/Task_1.java
+++ src/Task_1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class Main {
+public class Task_1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
