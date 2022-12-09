1b495a9ee01ce5b785fa69b650e461546ce9957e
-MaxPank- <101281041+MaxPankratov@users.noreply.github.com>
2022-12-09 10:38:10 +0300

    Update Task_1.java

--- src/Task_1.java
+++ src/Task_1.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class Task_1 {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите дату: ");
         String date = scanner.nextLine();
 
-        Pattern trueDatePattern = Pattern.compile("(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/19\\d{2}|[2-9]\\d{3}");
+        Pattern trueDatePattern = Pattern.compile("(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19\\d{2}|[2-9]\\d{3})");
         Matcher trueDateMatcher = trueDatePattern.matcher(date);
 
-        Pattern wrongFebruaryDayPattern = Pattern.compile("(3[01])/02/19\\d{2}|[2-9]\\d{3}"); //Для февраля
+        Pattern wrongFebruaryDayPattern = Pattern.compile("(3[01])/02/(19\\d{2}|[2-9]\\d{3})"); //Для февраля
         Matcher wrongFebruaryDayMatcher = wrongFebruaryDayPattern.matcher(date);
 
-        Pattern alternativeDatePattern = Pattern.compile("(0[1-9]|[12][0-9]|3[01])[-.\s,](0[1-9]|1[012])[-.\s,]19\\d{2}|[2-9]\\d{3}");
+        Pattern alternativeDatePattern = Pattern.compile("(0[1-9]|[12][0-9]|3[01])[-.\\s,](0[1-9]|1[012])[-.\\s,](19\\d{2}|[2-9]\\d{3})");
         Matcher alternativeDateMatcher = alternativeDatePattern.matcher(date);
 
         if ( trueDateMatcher.matches() && !(wrongFebruaryDayMatcher.matches()) ) {
