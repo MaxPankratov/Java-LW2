376b22f152ab826e20de3b6459e6f24e0352d35b
-MaxPank- <101281041+MaxPankratov@users.noreply.github.com>
2022-12-09 10:39:49 +0300

    Update Task_2.java

--- src/Task_2.java
+++ src/Task_2.java
@@ -1,19 +1,38 @@
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class Task_2 {
+public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите пароль: ");
-        String password = scanner.nextLine();
-        Pattern truePasswordPattern = Pattern.compile("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])._.{8,}");
+        
+        String password = "abc";
+        
+        Pattern truePasswordPattern = Pattern.compile("[a-zA-Z0-9_]{8,}");
         Matcher truePasswordMatcher = truePasswordPattern.matcher(password);
+        
+        Pattern uppercasePasswordPattern = Pattern.compile("[A-Z]");
+        Matcher uppercasePasswordMatcher = uppercasePasswordPattern.matcher(password);
+        
+        Pattern lowercasePasswordPattern = Pattern.compile("[a-z]");
+        Matcher lowerPasswordMatcher = lowercasePasswordPattern.matcher(password);
+        
+        Pattern digitPasswordPattern = Pattern.compile("[0-9]");
+        Matcher digitPasswordMatcher = digitPasswordPattern.matcher(password);
+        
+        boolean flag = false;
 
-        if (truePasswordMatcher.matches()) {
-            System.out.println("Пароль надёжен");
-        } else {
-            System.out.println("Пароль ненадёжен. Повторите ввод");
-        }
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите пароль: ");
+            password = scanner.nextLine();
+
+            if (truePasswordMatcher.matches() && uppercasePasswordMatcher.matches() && lowerPasswordMatcher.matches() && digitPasswordMatcher.matches()) {
+                flag = true;
+                System.out.println("Пароль надёжен");
+            } else {
+                flag = false;
+                System.out.println("Пароль ненадёжен. Повторите ввод");}
+            } while (flag == false);
+         
     }
 }
